13dcd1674f06cbb6623941cf4f44b56887b2efb0
MaksimZotov <fsfrgerG425>
2021-07-17 12:16:47 +0300

    working on UI

--- app/src/main/java/com/maksimzotov/habits/SecondActivity.kt
+++ app/src/main/java/com/maksimzotov/habits/SecondActivity.kt
@@ -1,9 +1,11 @@
 package com.maksimzotov.habits
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_second.*
+
 
 class SecondActivity : AppCompatActivity() {
 
@@ -14,6 +16,15 @@ class SecondActivity : AppCompatActivity() {
         save_habit.setOnClickListener {
             saveHabit()
         }
+
+        val drawable = GradientDrawable(
+            GradientDrawable.Orientation.BL_TR,
+            intArrayOf(Color.RED, Color.GREEN, Color.BLUE, Color.CYAN, Color.MAGENTA)
+        )
+        drawable.shape = GradientDrawable.RECTANGLE
+        drawable.gradientType = GradientDrawable.LINEAR_GRADIENT
+        drawable.cornerRadius = 40f
+        bg_colors.background = drawable
     }
 
     fun saveHabit() {
